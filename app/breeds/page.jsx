63cfdb885939850f51a5995ab760b60f3b59b74b
Ivan Khoroshylov <host513@gmail.com>
2023-08-21 15:05:47 +0300

    feat(routing): add breeds, gallery, voting pages

---
+++ app/breeds/page.jsx
@@ -0,0 +1,7 @@
+export default function Breeds() {
+  return (
+    <p>
+      Breeds Page
+    </p>
+  )
+}
---
+++ app/voting/page.jsx
@@ -0,0 +1,7 @@
+export default function Voting() {
+  return (
+    <p>
+      Voting Page
+    </p>
+  )
+}
