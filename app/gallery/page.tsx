930066415716e7224a425b145402618e097342a1
Ivan Khoroshylov <host513@gmail.com>
2023-08-21 19:12:08 +0300

    feat(gallery): add initial markup and styles for gallery page, inital fetch for 5 items

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,62 @@
-export default function Gallery() {
+import { NavigationButton } from '@/components/NavigationButton/NavigationButton';
+import styles from './page.module.scss'
+import classNames from 'classnames/bind';
+
+interface CatImage {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+const cn = classNames.bind(styles);
+
+async function getData() {
+  const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=5');
+ 
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+ 
+  const data: CatImage[] = await response.json();
+  return data;
+}
+
+export default async function Gallery() {
+  const data = await getData();
+
   return (
-    <p>
-      Gallery Page
-    </p>
+  <div>
+    <div className={cn('pageContent')}>
+
+      <div className={cn('breadCrumbs')}>
+        <div className={cn('backButton')}>
+          <NavigationButton link='/' text='<'/>
+        </div>
+
+        <div className={cn('currentPageButton')}>
+          <NavigationButton link='/gallery' text='Gallery'/>
+        </div>
+      </div>
+
+      <div className={cn('uploadButton')}>
+        <NavigationButton link='/' text='Upload'/>
+      </div>
+
+      <div className={cn('imagesContainer')}>
+        <ul className={cn('filterParams')}>
+          <li>Order</li>
+          <li>Type</li>
+          <li>Breed</li>
+          <li>Limit</li>
+          Refresh
+        </ul>
+
+        {data.length && data.map(item => (
+          <img className={cn('catImage')} key={item.id} src={item.url} alt='cat'/>
+        ))}
+      </div>
+    </div>
+  </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,28 @@
 import { Paw, PetsPaw } from "@/public/svg";
-import './page.scss'
-import { Button } from "@/components/Button/Button";
+import styles from './page.module.scss'
+import { NavigationButton } from "@/components/NavigationButton/NavigationButton";
+import classNames from 'classnames/bind';
+
+const cn = classNames.bind(styles);
 
 export default function Home() {
   return (
     <main>
-      <div className="logo">
+      <div className={cn('logo')}>
         <Paw/>
         <PetsPaw/>
       </div>
 
     <div>
-      <h1 className="title">Hi!👋</h1>
-      <h2 className="welcomeMessage">Welcome to MacPaw Bootcamp 2023</h2>
+      <h1 className={cn('title')}>Hi!👋</h1>
+      <h2 className={cn('welcomeMessage')}>Welcome to MacPaw Bootcamp 2023</h2>
     </div>
 
-      <div className="navigationLinks">
-        <h2 className="linksTitle">Lets start using The Cat API</h2>
-        <Button link="/voting" text="Voting"/>
-        <Button link="/breeds" text="Breeds"/>
-        <Button link="/gallery" text="Gallery"/>
+      <div className={cn('navigationLinks')}>
+        <h2 className={cn('linksTitle')}>Lets start using The Cat API</h2>
+        <NavigationButton link="/voting" text="Voting"/>
+        <NavigationButton link="/breeds" text="Breeds"/>
+        <NavigationButton link="/gallery" text="Gallery"/>
       </div>
     </main>
   )
