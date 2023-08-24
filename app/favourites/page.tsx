627e08c74751e98e0e57bc124c9804b408b9a2ef
Ivan Khoroshylov <host513@gmail.com>
2023-08-24 19:05:15 +0300

    feat(fav page): add initial fav page markup, getting and map fav pic

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+/* eslint-disable @next/next/no-img-element */
+'use client'
+
+import { Header } from '@/components/Header/Header';
+import React, { useEffect, useState } from 'react';
+import styles from './page.module.scss';
+import classNames from 'classnames/bind';
+import { type CatFavourite } from '@/types/favourites';
+import { getFav } from '@/utils/favourites';
+
+const cn = classNames.bind(styles)
 
 export default function Favourites () {
+  const [favourites, setFavourites] = useState<CatFavourite[]>([]);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    getFav('favourites').then((data) => { setFavourites(data) }).catch((error) => { setError(error) });
+  }, [])
+
   return (
-    <p>
-      Favourites Page
-    </p>
+    <div>
+      <Header/>
+      <div className={cn('contentContainer')}>
+      {error && (<div className={cn('error')}>{error}</div>)}
+      {favourites.length > 0 && favourites.map((favourite) => (
+        <div className={cn('favouriteContainer')} key={favourite.id}>
+          <img src={favourite.image.url} alt={'cat'} className={cn('favouriteImage')}/>
+        </div>
+      )
+      )}
+      </div>
+    </div>
   )
 }
